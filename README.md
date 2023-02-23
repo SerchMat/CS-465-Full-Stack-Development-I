c5b4a9de4ea342654b8b483bff5b3a490ea40f0b
SerchMat <serc.mat@gmail.com>
2023-02-23 10:40:37 -0600

    Convert travel template from static data to dynamic JSON

--- README.md
+++
@@ -1,2 +0,0 @@
-# CS-465-Full-Stack-Development-I
-CS 465 Full Stack Development with MEAN 
--- app.js
+++ app.js
@@ -3,17 +3,19 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const hbs = require('hbs');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./app_server/routes/index');
+const usersRouter = require('./app_server/routes/users');
+const travelRouter = require('./app_server/routes/travel');
 
 const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'app_server', 'views'));
 
-// register handlebars partials (https://www.npmjs.com/package/hbs)
-hbs.registerPartials(path.join(__dirname, 'app_server', 'views/partials'));
+// register handlebars partials(https://www.npmjs.com/package/hbs)
+hbs.registerPartials(path.join(__dirname, 'app_server','views/partials'));
 
 app.set('view engine', 'hbs');
 
@@ -22,6 +24,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
